0e5463754a0f2afef74dc2435d78bdd70ee9dbd9
lizhaoyun <2571770878@qq.com>
2020-04-07 20:51:46 +0800

    目的地

---
+++ 李召云/src/culture/cultureDetail.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {View,Text} from 'react-native';
+
+export default class cultureDetail extends Component {
+    render() {
+        return (
+            <View>
+                <Text>详情</Text>
+            </View>
+        )
+    }
+}
