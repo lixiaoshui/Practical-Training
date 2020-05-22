c8b991c92f748c82994f67d6f9f0e039742f9d88
lixiaoshui <2087751927@qq.com>
2020-05-22 22:25:17 +0800

    lj

--- 李佳/src/community/Community.js
+++ 李佳/src/community/Community.js
@@ -87,7 +87,7 @@ export default class Community extends Component {
         }
     }
     componentDidMount(){
-        fetch('/dt/list')
+        fetch('http://192.168.0.104:3001/dt/list')
         .then(res=>res.json())
         .then(res=>{
             console.log(res);
@@ -101,7 +101,7 @@ export default class Community extends Component {
     componentDidUpdate(prevProps,prevState){
         if(prevState.data==this.state.data){
             console.log(prevState.data==this.state.data)
-            fetch('/dt/list')
+            fetch('http://192.168.0.104:3001/dt/list')
             .then(res=>res.json())
             .then(res=>{
                 console.log(res);
@@ -159,12 +159,22 @@ export default class Community extends Component {
                             </TouchableOpacity>
                             <TouchableOpacity onPress={Actions.dtcontent} >
                                 <View style={styles.con}  >
-                                    <Image style={styles.toux} source={require('../../assets/lj/ljtouxiang2.jpg')} />
-                                    <Text style={styles.adminname} >小知</Text>
+                                    <Image style={styles.toux} source={require('../../assets/lj/ljtouxiang3.jpg')} />
+                                    <Text style={styles.adminname} >小行</Text>
                                     <Text style={styles.admintime} >2020.02.10</Text>
                                     <Text style={styles.admincon} >
-                                        这次衡山行，最有感觉的几处地方，麻姑仙境、石浪、祝孔庙于等，这次衡山行，最有感觉的几处地方，麻姑仙境、石浪、祝孔庙于等
-                                        </Text>
+                                    有时我们选择改变，并非经过深思熟虑，而更像是听见了天地间冥冥中的呼唤，呼唤你前往另一个地方，过上另一种生活。
+                                    </Text>
+                                </View>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={Actions.dtcontent} >
+                                <View style={styles.con}  >
+                                    <Image style={styles.toux} source={require('../../assets/lj/ljtouxiang1.jpeg')} />
+                                    <Text style={styles.adminname} >知行</Text>
+                                    <Text style={styles.admintime} >2020.02.10</Text>
+                                    <Text style={styles.admincon} >
+                                    人生就像是一场旅行，遇到的既有感人的，也有伤心的，既有令人兴奋的，也有令人灰心的，既有美妙的风景，也会有称不上景只有风的地方。
+                                    </Text>
                                 </View>
                             </TouchableOpacity>
                             <TouchableOpacity onPress={Actions.dtcontent} >
@@ -173,41 +183,30 @@ export default class Community extends Component {
                                     <Text style={styles.adminname} >小知</Text>
                                     <Text style={styles.admintime} >2020.02.10</Text>
                                     <Text style={styles.admincon} >
-                                        这次衡山行，最有感觉的几处地方，麻姑仙境、石浪、祝孔庙于等，这次衡山行，最有感觉的几处地方，麻姑仙境、石浪、祝孔庙于等
-                                        </Text>
+                                    有时间，我们可以去爬山，去看日出，去散步，去欣赏大自然，去陌生的街角，去做一切我们曾经或现在也很想做的事情，可以有一个人陪着你，也可以你一个人。
+                                    </Text>
                                 </View>
                             </TouchableOpacity>
                             <TouchableOpacity onPress={Actions.dtcontent} >
                                 <View style={styles.con}  >
-                                    <Image style={styles.toux} source={require('../../assets/lj/ljtouxiang2.jpg')} />
-                                    <Text style={styles.adminname} >小知</Text>
+                                    <Image style={styles.toux} source={require('../../assets/lj/ljtouxiang1.jpeg')} />
+                                    <Text style={styles.adminname} >知行</Text>
                                     <Text style={styles.admintime} >2020.02.10</Text>
                                     <Text style={styles.admincon} >
-                                        这次衡山行，最有感觉的几处地方，麻姑仙境、石浪、祝孔庙于等，这次衡山行，最有感觉的几处地方，麻姑仙境、石浪、祝孔庙于等
-                                        </Text>
+                                    失去的我们不妨让其失去，因为它可让我们少些惆怅;得到的我们不妨少些满足，因为它可让我们多些清醒。
+                                    </Text>
                                 </View>
                             </TouchableOpacity>
                             <TouchableOpacity onPress={Actions.dtcontent} >
                                 <View style={styles.con}  >
-                                    <Image style={styles.toux} source={require('../../assets/lj/ljtouxiang2.jpg')} />
-                                    <Text style={styles.adminname} >小知</Text>
-                                    <Text style={styles.admintime} >2020.02.10</Text>
-                                    <Text style={styles.admincon} >
-                                        这次衡山行，最有感觉的几处地方，麻姑仙境、石浪、祝孔庙于等，这次衡山行，最有感觉的几处地方，麻姑仙境、石浪、祝孔庙于等
-                                        </Text>
-                                </View>
-                            </TouchableOpacity>
-                            <TouchableOpacity onPress={Actions.dtcontent} >
-                                <View style={styles.con}  >
-                                    <Image style={styles.toux} source={require('../../assets/lj/ljtouxiang2.jpg')} />
-                                    <Text style={styles.adminname} >小知</Text>
+                                    <Image style={styles.toux} source={require('../../assets/lj/ljtouxiang3.jpg')} />
+                                    <Text style={styles.adminname} >小行</Text>
                                     <Text style={styles.admintime} >2020.02.10</Text>
                                     <Text style={styles.admincon} >
                                         这次衡山行，最有感觉的几处地方，麻姑仙境、石浪、祝孔庙于等，这次衡山行，最有感觉的几处地方，麻姑仙境、石浪、祝孔庙于等
                                         </Text>
                                 </View>
                             </TouchableOpacity> */}
-
                         </View>
                     </ScrollView>
                 </ImageBackground>
