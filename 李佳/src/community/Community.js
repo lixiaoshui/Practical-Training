2a16bfc663d07e88e50f139888931a9f656752fc
lixiaoshui <2087751927@qq.com>
2020-06-10 09:03:36 +0800

    lj

--- 李佳/src/community/Community.js
+++ 李佳/src/community/Community.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View, Image, ScrollView, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native'
+import { DeviceEventEmitter,Text, View, Image, ScrollView, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native'
 import { Actions } from 'react-native-router-flux';
 import { Button } from '@ant-design/react-native';
 
@@ -36,8 +36,10 @@ const styles = StyleSheet.create({
     },
     body: {
         width: '100%',
+        height:"90%",
         alignItems: 'center',
         justifyContent: 'center',
+        marginTop:60
         // height:400,
         // borderWidth:1
     },
@@ -79,14 +81,18 @@ const styles = StyleSheet.create({
     }
 })
 
+
 export default class Community extends Component {
     constructor(){
         super();
         this.state={
-            data:[]
+            data:[],
+            username:'admin'
+            // data:am
         }
     }
     componentDidMount(){
+        // fetch('http://192.168.0.104:3001/dt/list')
         fetch('http://192.168.0.104:3001/dt/list')
         .then(res=>res.json())
         .then(res=>{
@@ -96,6 +102,20 @@ export default class Community extends Component {
             },()=>{
                 console.log(this.state.data)
             })
+        })
+        DeviceEventEmitter.addListener("returnContent", (params) => {
+            var con =JSON.parse(params);
+            console.log(con.username+":"+con.content + "获取发表内容00000000");
+            // var lista=[con,...this.state.data];
+            // console.log(lista);
+            this.setState((state)=>{
+                return {
+                    data:[con,...state.data]
+                }
+            },()=>{
+                console.log("ok"+this.state.data);
+            })
+            // console.log()
         })
     }
     componentDidUpdate(prevProps,prevState){
@@ -130,13 +150,13 @@ export default class Community extends Component {
                 </ImageBackground>
                 {/* body */}
                 <ImageBackground source={require('../../assets/lj/ljbodybg.jpg')} style={{ width: '100%', height: 718 }} >
-                    <ScrollView>
+                    <ScrollView >
                         <View style={styles.body} >
                             {
                                 this.state.data.map((item,idx)=>{
-                                    return <TouchableOpacity onPress={Actions.dtcontent} >
+                                    return <TouchableOpacity onPress={()=>Actions.dtcontent({picpath:item.picpath,username:item.username,content:item.content,createtime:item.createtime})} >
                                         <View style={styles.con} >
-                                            <Image style={styles.toux} source={item.imgpath} />
+                                            <Image style={styles.toux} source={{uri:item.picpath}} />
                                             <Text style={styles.adminname} > {item.username} </Text>
                                             <Text style={styles.admintime} > {item.createtime} </Text>
                                             <Text style={styles.admincon} > {item.content} </Text>
@@ -144,69 +164,6 @@ export default class Community extends Component {
                                     </TouchableOpacity>
                                 })
                             }
-
-
-
-                            {/* <TouchableOpacity onPress={Actions.dtcontent} >
-                                <View style={styles.con}  >
-                                    <Image style={styles.toux} source={require('../../assets/lj/ljtouxiang2.jpg')} />
-                                    <Text style={styles.adminname} >小知</Text>
-                                    <Text style={styles.admintime} >2020.02.10</Text>
-                                    <Text style={styles.admincon} >
-                                        这次衡山行，最有感觉的几处地方，麻姑仙境、石浪、祝孔庙于等，这次衡山行，最有感觉的几处地方，麻姑仙境、石浪、祝孔庙于等
-                                        </Text>
-                                </View>
-                            </TouchableOpacity>
-                            <TouchableOpacity onPress={Actions.dtcontent} >
-                                <View style={styles.con}  >
-                                    <Image style={styles.toux} source={require('../../assets/lj/ljtouxiang3.jpg')} />
-                                    <Text style={styles.adminname} >小行</Text>
-                                    <Text style={styles.admintime} >2020.02.10</Text>
-                                    <Text style={styles.admincon} >
-                                    有时我们选择改变，并非经过深思熟虑，而更像是听见了天地间冥冥中的呼唤，呼唤你前往另一个地方，过上另一种生活。
-                                    </Text>
-                                </View>
-                            </TouchableOpacity>
-                            <TouchableOpacity onPress={Actions.dtcontent} >
-                                <View style={styles.con}  >
-                                    <Image style={styles.toux} source={require('../../assets/lj/ljtouxiang1.jpeg')} />
-                                    <Text style={styles.adminname} >知行</Text>
-                                    <Text style={styles.admintime} >2020.02.10</Text>
-                                    <Text style={styles.admincon} >
-                                    人生就像是一场旅行，遇到的既有感人的，也有伤心的，既有令人兴奋的，也有令人灰心的，既有美妙的风景，也会有称不上景只有风的地方。
-                                    </Text>
-                                </View>
-                            </TouchableOpacity>
-                            <TouchableOpacity onPress={Actions.dtcontent} >
-                                <View style={styles.con}  >
-                                    <Image style={styles.toux} source={require('../../assets/lj/ljtouxiang2.jpg')} />
-                                    <Text style={styles.adminname} >小知</Text>
-                                    <Text style={styles.admintime} >2020.02.10</Text>
-                                    <Text style={styles.admincon} >
-                                    有时间，我们可以去爬山，去看日出，去散步，去欣赏大自然，去陌生的街角，去做一切我们曾经或现在也很想做的事情，可以有一个人陪着你，也可以你一个人。
-                                    </Text>
-                                </View>
-                            </TouchableOpacity>
-                            <TouchableOpacity onPress={Actions.dtcontent} >
-                                <View style={styles.con}  >
-                                    <Image style={styles.toux} source={require('../../assets/lj/ljtouxiang1.jpeg')} />
-                                    <Text style={styles.adminname} >知行</Text>
-                                    <Text style={styles.admintime} >2020.02.10</Text>
-                                    <Text style={styles.admincon} >
-                                    失去的我们不妨让其失去，因为它可让我们少些惆怅;得到的我们不妨少些满足，因为它可让我们多些清醒。
-                                    </Text>
-                                </View>
-                            </TouchableOpacity>
-                            <TouchableOpacity onPress={Actions.dtcontent} >
-                                <View style={styles.con}  >
-                                    <Image style={styles.toux} source={require('../../assets/lj/ljtouxiang3.jpg')} />
-                                    <Text style={styles.adminname} >小行</Text>
-                                    <Text style={styles.admintime} >2020.02.10</Text>
-                                    <Text style={styles.admincon} >
-                                        这次衡山行，最有感觉的几处地方，麻姑仙境、石浪、祝孔庙于等，这次衡山行，最有感觉的几处地方，麻姑仙境、石浪、祝孔庙于等
-                                        </Text>
-                                </View>
-                            </TouchableOpacity> */}
                         </View>
                     </ScrollView>
                 </ImageBackground>
